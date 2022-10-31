54c22e825c666f90683fd836a522174e0434968d
Manuel Jiménez <manuel@jimenez.eu.org>
2022-10-31 18:22:18 +0100

    Shareable URLs in results

--- app.py
+++ app.py
@@ -12,12 +12,13 @@ Bootstrap5(app)
 def hello():    
     search = AirportForm(request.form)
     if request.method == 'POST':
-        return result(search.airport, search.d00, search.d01, search.d10, search.d11)
+        #return result(search.airport.data, search.d00.data, search.d01.data, search.d10.data, search.d11.data)
+        return redirect("/" + str(search.airport.data) + "/" + str(search.d00.data) + "/" + str(search.d01.data) + "/" + str(search.d10.data) + "/" + str(search.d11.data))
     return render_template("index.html", form=search)
 
-@app.route("/<airport>/<d00>/<d01>/<d10>/<d11>") 
+@app.route("/<airport>/<d00>/<d01>/<d10>/<d11>")
 def result(airport, d00, d01, d10, d11):
-    trips = ryanair.get_return_flights(airport.data, str(d00.data), str(d01.data), str(d10.data), str(d11.data))
+    trips = ryanair.get_return_flights(str(airport), str(d00), str(d01), str(d10), str(d11))
     return render_template("show_list.html", data = trips)
 
 if __name__ == "__main__": 
