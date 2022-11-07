2c528a53b2398f2650af7e0a028a838d16c234d2
Manuel Jiménez <manuel@jimenez.eu.org>
2022-11-07 23:32:17 +0100

    New start page

--- forms.py
+++ forms.py
@@ -3,7 +3,7 @@ from datetime import date
 import datetime
 
 class AirportForm(Form):
-    next_thu = date.today() + datetime.timedelta(days=((3 - date.today().weekday()) % 7))
+    next_thu = date.today() + datetime.timedelta(days=(21 + (3 - date.today().weekday()) % 7))
     airport = StringField(label="Airport ", default="MAD")
     d00 = DateField(label="between", default=next_thu)
     d01 = DateField(label = "and", default=next_thu + datetime.timedelta(days=1))
